b83b74e62d209f8a1f9a48c0b1fae5ed4afcbf13
LiaKitamura <kit4mura@gmail.com>
2015-02-16 21:16:37 -0700

    fix js not working

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
   // caching so we can keep using. the $ in front of mobileNav is just to tell me its a jQuery element
   var $mobileNav = $('#mobile-nav'),
       $menuLink = $mobileNav.find('a#mobile-nav-link'),
-      $mobileNavLink = $('#mobile-nav').find('ul#mobile-nav-list');
+      $mobileNavLinks = $('#mobile-nav').find('ul#mobile-nav-list');
 
   $menuLink.click(function(event){
     event.preventDefault();
-    $mobileNavLink.slideToggle();
+    $mobileNavLinks.slideToggle();
   });
 
 });
